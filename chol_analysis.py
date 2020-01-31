893c4520ad7adcd4b600415706cbfa79468b9fc7
Baiying Lu <bl223@duke.edu>
2020-01-31 12:48:06 -0500

    Add a new fever branch

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,14 +1,58 @@
-# class 4
+def HDL_anaysis(HDL_level):
+    print("This is HDL level test")
+    if HDL_level >= 60:
+        return "Normal"
+    elif 40 <= HDL_level < 60:
+        return "Borderline low"
+    else:
+        return "Low"
+def LDL_anaysis(LDL_level):
+    print("This is LDL level test")
+    if LDL_level < 130:
+        return "Normal"
+    elif 130 <= LDL_level < 159:
+        return "Borderline low"
+    elif 159 <= LDL_level < 189:
+        return "High"
+    else:
+        return "Very High"
+def Cholestrol_overall_level(Chol_o_level):
+    print("This is the cholestrol overall level")
+    if Chol_o_level < 200:
+        return "Normal"
+    elif 200 <= Chol_o_level < 240:
+        return "Borderline high"
+    else:
+        return "High"
+def Cholestrol_analysis():
+    print("Cholestrol analysis")
+    HDLinput = input("Enter test result:")
+    test_info = HDLinput.split('=')
+    if test_info[0] == "HDL":
+        answer = HDL_anaysis(int(test_info[1]))
+        print("The level is {}".format(answer))
+    if test_info[0] == "LDL":
+        answer = LDL_anaysis(int(test_info[1]))
+        print("The level is {}".format(answer))
+    if test_info[0] == "COA":
+        answer = LDL_anaysis(int(test_info[1]))
+        print("The level is {}".format(answer))
 def interface():
     while True:
-#    choice = 0
-#    while choice != 9:
         print("Cholestrol calculator")
         print("options:")
+        print("1 -do analysis")
         print("9 - quit")
-        choice = input('ENter you option:')
+        choice = input('Enter you option:')
         if choice == '9':
             return
-
+        elif choice == "1":
+            Cholestrol_analysis()
+def fever_check(temp_list):
+    fever = False
+    for temperature in temp_list:
+        if temperature > 98.6:
+            fever = True
+    return fever
 if __name__=="__main__":
     interface()
---
+++ test_chol_HDL_analysis.py
@@ -0,0 +1,20 @@
+def test_HDL_analysis_normal():
+    from chol_analysis import HDL_anaysis
+    answer = HDL_anaysis(80)
+    expected = "Normal"
+    assert answer == expected#look for  assertion error to see if it is correct
+def test_HDL_analysis_bl():
+    from chol_analysis import HDL_anaysis
+    answer = HDL_anaysis(40)
+    expected = "Borderline low"
+    assert answer == expected
+def test_LDL_anaysis():
+    from chol_analysis import LDL_anaysis
+    answer = LDL_anaysis(150)
+    expected = "Borderline low"
+    assert answer == expected
+def test_fever_check():
+    from chol_analysis import fever_check
+    new_data = [96.0,100,105.1,97]
+    answer = fever_check(new_data)
+    exepcted = "Normal"
